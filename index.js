8aa66f2b29e9b2286f0677a4cfa3e1ae46c82c13
Simen Brekken <simen@unfold.no>
2014-07-30 10:37:02 +0200

    Update dependencies and simplify.

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 var loaderUtils = require('loader-utils');
 var autoprefixer = require('autoprefixer');
-var path = require('path');
-var fs = require('fs');
 
-module.exports = function (source) {
-  if (this.cacheable) {
-    this.cacheable();
-  }
+module.exports = function(source) {
+  this.cacheable && this.cacheable();
 
   var options = loaderUtils.parseQuery(this.query);
-  var processed;
-  var browsers = ['> 1%', 'last 2 versions',
-  'Firefox ESR', 'Opera 12.1'];
-
-  if (options.browsers) {
-    if (Array.isArray(options.browsers)) {
-      browsers = options.browsers;
-    } else {
-      browsers = options.browsers.split(',');
-    }
-  }
-
-  processed = autoprefixer.apply(autoprefixer, browsers)
-    .process(source);
+  var browsers = options.browsers ? options.browsers.split(/,\s*/) : null;
+  var processed = autoprefixer(browsers).process(source);
 
   this.callback(null, processed.css, processed.map);
 };
